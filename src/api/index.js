9963fa43b53f59b136d864602a8f7775bab61007
damu <damu@example.com>
2020-07-08 19:43:57 +0800

    day02

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,16 @@
 // 包含多个接口请求函数的模块,函数返回值：promise对象
 import ajax from './ajax'
 
+const BASE_URL = '/api'
+
 // 获取地址信息(根据经纬度串)
-export const reqAddress = (geohash) => ajax('/position/${geohash}')
+export const reqAddress = (geohash) => ajax('${BASE_URL}/position/${geohash}')
 
 // 获取msite页面食品分类列表
-export const reqFoodTypes = () => ajax('/index_category')
+export const reqFoodCategorys = () => ajax(BASE_URL + '/index_category')
 
 // 获取msite商铺列表(根据经纬度)
-export const reqShops = (latitude, longitude) => ajax('/shops', {latitude, longitude})
+export const reqShops = (latitude, longitude) => ajax(BASE_URL + '/shops', {latitude, longitude})
 
 // 账号密码登录
 
