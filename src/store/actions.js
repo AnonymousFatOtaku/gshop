8877f5674d41cc556ae8fea86d9ea9439611b841
damu <damu@example.com>
2020-07-13 19:59:30 +0800

    商家界面顶部及部分点餐

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,6 +5,9 @@ import {
   RECEIVE_SHOPS,
   RECEIVE_USER_INFO,
   RESET_USER_INFO,
+  RECEIVE_GOODS,
+  RECEIVE_RATINGS,
+  RECEIVE_INFO,
 } from './mutation-types'
 import {
   reqAddress,
@@ -12,6 +15,9 @@ import {
   reqShops,
   reqUserInfo,
   reqLogout,
+  reqShopRatings,
+  reqShopGoods,
+  reqShopInfo,
 } from '../api'
 
 export default {
@@ -64,6 +70,30 @@ export default {
     const result = await reqLogout()
     if (result.code === 0) {
       commit(RESET_USER_INFO)
+    }
+  },
+  // 异步获取商家信息
+  async getShopInfo({commit}) {
+    const result = await reqShopInfo()
+    if (result.code === 0) {
+      const info = result.data
+      commit(RECEIVE_INFO, {info})
+    }
+  },
+  // 异步获取商家评价列表
+  async getShopRatings({commit}) {
+    const result = await reqShopRatings()
+    if (result.code === 0) {
+      const ratings = result.data
+      commit(RECEIVE_RATINGS, {ratings})
+    }
+  },
+  // 异步获取商家商品列表
+  async getShopGoods({commit}) {
+    const result = await reqShopGoods()
+    if (result.code === 0) {
+      const goods = result.data
+      commit(RECEIVE_GOODS, {goods})
     }
   },
 }
