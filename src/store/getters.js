f39c8123a88d35e38d21c5c2ebbafedbf1743dcc
damu <damu@example.com>
2020-07-15 19:28:09 +0800

    删减购物车商品

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,11 @@
 // 包含多个基于state的getter计算属性的对象
 export default {
-
+  // 购物车商品总数
+  totalCount(state) {
+    return state.cartFoods.reduce((preTotal, food) => preTotal + food.count, 0)
+  },
+  // 购物车商品总价
+  totalPrice(state) {
+    return state.cartFoods.reduce((preTotal, food) => preTotal + food.count * food.price, 0)
+  },
 }
