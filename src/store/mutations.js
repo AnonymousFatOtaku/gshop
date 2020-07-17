3dea578e6572a0fae6ba4b75f7e281a8525dd7e5
damu <damu@example.com>
2020-07-17 16:45:49 +0800

    搜索商家

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -11,6 +11,7 @@ import {
   INCREMENT_FOOD_COUNT,
   DECREMENT_FOOD_COUNT,
   CLEAR_CART,
+  RECEIVE_SEARCH_SHOPS
 } from './mutation-types'
 import Vue from 'vue'
 
@@ -73,5 +74,9 @@ export default {
     state.cartFoods.forEach(food => food.count = 0)
     // 移除购物车中所有购物项
     state.cartFoods = []
+  },
+
+  [RECEIVE_SEARCH_SHOPS](state, {searchShops}) {
+    state.searchShops = searchShops
   },
 }
