412d7834a976670bac5686fe9279179a09bd9e89
damu <damu@example.com>
2020-07-17 18:19:19 +0800

    路由组件及图片懒加载

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import router from './router'
 import store from './store'
 import {Button} from 'mint-ui'
 import './mock/mockServer' // 加载mockServer即可
+import VueLazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
 
 // 注册全局组件标签
 Vue.component(Button.name, Button)  // <mt-button>
+
+// 内部自定义一个指令lazy
+Vue.use(VueLazyload, {
+  loading
+})
 
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 // 路由器对象模块
 import Vue from 'vue'
 import VueRouter from "vue-router"
-import MSite from '../pages/MSite/MSite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
-import Login from '../pages/Login/Login.vue'
-import Shop from '../pages/Shop/Shop.vue'
-import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
-import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
-import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+
+// 路由组件懒加载，一般应用于模块较多较大的组件，小的可以不用避免浪费请求
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+const Login = () => import('../pages/Login/Login.vue')
+const Shop = () => import('../pages/Shop/Shop.vue')
+const ShopGoods = () => import('../pages/Shop/ShopGoods/ShopGoods.vue')
+const ShopRatings = () => import('../pages/Shop/ShopRatings/ShopRatings.vue')
+const ShopInfo = () => import('../pages/Shop/ShopInfo/ShopInfo.vue')
 
 // 声明使用插件
 Vue.use(VueRouter)
@@ -20,7 +22,7 @@ export default new VueRouter({
   routes: [
     {
       path: '/msite',
-      component: MSite,
+      component: MSite, // 返回路由组件的函数,只有执行此函数才会加载路由组件,这个函数在请求对应的路由路径时才会执行
       meta: {
         showFooter: true
       }
